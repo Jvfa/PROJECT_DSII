8e34b7f1e0ac13ef7a291d5ac3f2b6ee920cca50
Jvfa <joao.victor1feneriche@gmail.com>
2025-05-30 20:40:22 -0300

    add

--- CadastroNinja/src/main/java/br/com/viladafolha/CadastroNinja/Controller/NinjaController.java
+++ CadastroNinja/src/main/java/br/com/viladafolha/CadastroNinja/Controller/NinjaController.java
@@ -1,17 +1,30 @@
 package br.com.viladafolha.CadastroNinja.Controller;
 
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
+import br.com.viladafolha.CadastroNinja.Entity.NinjaEntity;
+import br.com.viladafolha.CadastroNinja.Service.NinjaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
 @RequestMapping("/api/ninja")
 public class NinjaController {
+
+    @Autowired
+    private NinjaService ninjaService;
+
     @GetMapping("/boasvindas")
     public String ninjaBoasVindas(){
         return "Ola voce acessou o sistema de cadastro de ninjas";
     }
+
+
+    //Cadastrar
+    @PostMapping("/cadastrar")
+    public NinjaEntity cadastrar(@RequestBody NinjaEntity ninja){
+        return ninjaService.cadastrar(ninja);
+    }
+
 }
+
--- CadastroNinja/src/main/java/br/com/viladafolha/CadastroNinja/Service/NinjaService.java
+++ CadastroNinja/src/main/java/br/com/viladafolha/CadastroNinja/Service/NinjaService.java
@@ -1,9 +1,17 @@
 package br.com.viladafolha.CadastroNinja.Service;
 
 
+import br.com.viladafolha.CadastroNinja.Entity.NinjaEntity;
+import br.com.viladafolha.CadastroNinja.Repository.NinjaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class NinjaService {
+    @Autowired
+    private NinjaRepository ninjaRepository;
 
+    public NinjaEntity cadastrar(NinjaEntity ninja){
+        return ninjaRepository.save(ninja);
+    }
 }
